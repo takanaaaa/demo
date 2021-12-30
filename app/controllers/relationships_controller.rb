e33f586a99bbf691961289ec885df0f155a74354
EC2 Default User <ec2-user@ip-172-31-85-21.ec2.internal>
2021-12-30 15:06:36 +0000

    [Add]followers/followings

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -8,4 +8,14 @@ class RelationshipsController < ApplicationController
     current_user.unfollow(params[:id])
     @user = User.find(params[:id])
   end
+  
+  def followings
+    user = User.find(params[:user_id])
+    @users = user.following_user
+  end
+
+  def followers
+    user = User.find(params[:user_id])
+    @users = user.follower_user
+  end
 end
