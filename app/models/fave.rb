acb35d9ee434a16d8eee2ded431e818ee2874eed
EC2 Default User <ec2-user@ip-172-31-85-21.ec2.internal>
2022-01-03 08:25:49 +0000

    [Fix]layouts

--- app/models/fave.rb
+++ app/models/fave.rb
@@ -2,6 +2,7 @@ class Fave < ApplicationRecord
   has_many :groups, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :fave_images, dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :user
 
   attachment :image
 
